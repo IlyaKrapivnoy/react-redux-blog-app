5bf9aa6d6cfecaf6339f3b776ae0e0bc1ddbfc97
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-13 20:09:09 +0300

    delete post func

--- src/store/reducers/postReducer.js
+++ src/store/reducers/postReducer.js
@@ -1,25 +1,21 @@
 const initState = {
     posts: [
-        {
-            id: 1,
-            title: 'post one',
-            content: 'this is post one',
-        },
-        {
-            id: 2,
-            title: 'post two',
-            content: 'this is post two',
-        },
-        {
-            id: 3,
-            title: 'post three',
-            content: 'this is post three',
-        },
+        { id: 1, title: 'post one', content: 'this is post one' },
+        { id: 2, title: 'post two', content: 'this is post two' },
+        { id: 3, title: 'post three', content: 'this is post three' },
     ],
 };
 
 const postReducer = (state = initState, action) => {
-    return state;
+    switch (action.type) {
+        case 'DELETE_POST':
+            const newPost = state.posts.filter((post) => post.id !== action.id);
+            return {
+                posts: newPost,
+            };
+        default:
+            return state;
+    }
 };
 
 export default postReducer;
