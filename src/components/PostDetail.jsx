b7677b99545f39729b2b4ae238bcf40cf0f789b1
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-13 19:39:19 +0300

    display data from store

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 
 class PostDetail extends Component {
     render() {
+        console.log(this.props);
         return (
             <div className='col s12 m6'>
                 <div className='card'>
                     <div className='card-content'>
-                        <span className='card-title'>Post One</span>
-                        <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Ratione eveniet in corrupti.
-                        </p>
+                        <span className='card-title'>
+                            {this.props.post.title}
+                        </span>
+                        <p>{this.props.post.content}</p>
                     </div>
                     <div className='card-action'>
                         <button className='btn red'>Delete</button>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import PostDetail from './PostDetail';
+import { connect } from 'react-redux';
 
 class PostList extends Component {
     render() {
+        const { posts } = this.props;
         return (
             <div className='container'>
                 <div className='row'>
-                    <PostDetail />
-                    <PostDetail />
-                    <PostDetail />
-                    <PostDetail />
+                    {posts &&
+                        posts.map((post) => (
+                            <PostDetail post={post} key={post.id} />
+                        ))}
                 </div>
             </div>
         );
     }
 }
 
-export default PostList;
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts,
+    };
+};
+
+export default connect(mapStateToProps)(PostList);
